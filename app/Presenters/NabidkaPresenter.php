848061957b6e9feadd1916c306e025aab9c14ffa
Damis <adam.smehyl@seznam.cz>
2020-04-18 19:25:22 +0200

    Offer listing successful

--- app/Presenters/NabidkaPresenter.php
+++ app/Presenters/NabidkaPresenter.php
@@ -5,22 +5,25 @@ declare(strict_types = 1);
 namespace App\Presenters;
 
 use App\Model;
+
 use Tracy\Debugger;
 
 final class NabidkaPresenter extends BasePresenter {
 
     private $nabidkaManager;
 
-    public function _construct(Model\NabidkaManager $nabidkaManager) {
+    public function __construct(Model\NabidkaManager $nabidkaManager) {
         $this->nabidkaManager = $nabidkaManager;
     }
 
-    public function renderList($order = 'datum DESC'): void {
+    public function renderList($order = 'datum ASC'): void {
         Debugger:bdump('Proměnná order: ', $order);
+        $this->template->nabidkaList = $this->nabidkaManager->getAll($order);
     }
 
     public function renderDetail($id): void {
         Debugger:bdump('Proměnná id: ', $id);
+        $this->template->nabidka = $this->nabidkaManager->getById($id);
     }
 
     public function actionInsert(): void {
